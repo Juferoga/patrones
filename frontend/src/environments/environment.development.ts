03cbe8d674e10dbc52bf840c35eeadd4efaf0f71
juferoga <juferoga@gmail.com>
2023-06-14 18:07:48 -0500

    JR - [ADD] template project

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  docs:'https://juferoga.github.io/patrones',
+  cronograma: 'https://github.com/users/Juferoga/projects/2/views/3',
+  repositorio: 'https://github.com/juferoga/patrones',
+  juferoga: 'https://github.com/juferoga/',
+  integrantes: [
+    {'name':'Brayan Yate','code':'20171020000','url':'https://github.com/juferoga'},
+    {'name':'Juan Felipe Rodríguez','code':'20181020158','url':'https://github.com/juferoga'}
+  ],
+  materia: 'PATRONES',
+  proyecto: 'proyecto',
+  profesor: 'Helio Ramirez',
+  api: 'http://localhost:8000/api/',
+  api_factura: 'http://localhost:8000/',
+  server: 'http://localhost:8000/',
+};
